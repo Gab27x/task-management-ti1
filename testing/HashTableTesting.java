4b469299a6e619fb120acc44462b20bcebec7d30
Lrojas898 <luis.manuel.rojas71@gmail.com>
2023-09-27 17:52:42 -0500

    Testing Hash table resolved

--- testing/HashTableTesting.java
+++ testing/HashTableTesting.java
@@ -1,192 +1,96 @@
-import model.HashTable;
+import model.*;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-/*
-Most of your mistakes are for not using the appropriated structure for the test
-also some test are redundant and unnecesary
-be careful with raw data please fix the structure
 
-*/
 public class HashTableTesting {
 
     private HashTable<Integer,Integer> hashTableTest;
-    public HashTableTesting(){
+    public void HashTableTesting(){
         hashTableTest = new HashTable<>();
     }
 
 
-/*    public void setUp() {
-        hashTableTest = new HashTable<>();
-    }
-    public void setUp2() {
-        hashTableTest = new HashTable<>(100);
-    }*/
 
-    //fixed
     @Test
-    public void testConstructorMethodEmptyList(){
-
-        assertTrue(hashTableTest.isEmpty());
-    }
-    //fixed
-    @Test
-    public void testConstructorMethodEmptyListGivenSize(){
-        // defult size is 200
-        assertEquals(200, hashTableTest.getDefaultSize());
-    }
-
-    //fixed
-    @Test
-    public void testAddMethodEmptyList(){
+    void addTest(){
+        HashTableTesting();
         hashTableTest.add(1,2);
         assertFalse(hashTableTest.isEmpty());
+
     }
 
-
-
-    // unnecesary test only keep one
-
-/*    @Test
-    public void testAddMethodEmptyListTwoElementsInserted(){
-        setUp();
-        hashTableTest.add(1,2);
-        hashTableTest.add(2,4);
-        assertFalse(hashTableTest.isEmpty());
-    }*/
-
-    // i dont see the point of this test
-/*    @Test
-    public void testAddMethodEmptyListGivenSize(){
-        setUp2();
-        hashTableTest.add(1,2);
-        assertFalse(hashTableTest.isEmpty());
-    }*/
-
-
-// redundant test
-/*    @Test
-    public void testAddMethodEmptyListGivenSizeTwoElementsInserted(){
-        setUp2();
-        hashTableTest.add(1,2);
-        hashTableTest.add(2,4);
-        assertFalse(hashTableTest.isEmpty());
-    }*/
-
-// the test is bad designed
-/*    @Test
-    public void testAddMethodEmptyListCollisions(){
-
-        hashTableTest.add(1,2);
-        hashTableTest.add(1,4);
-        assertEquals(hashTableTest.find(1,4).getValue(),4);
-    }*/
-    // correction
-    @Test
-    public void testAddMethodEmptyListCollisions(){
-
-        hashTableTest.add(1,2);
-        hashTableTest.add(1,4);
-        // Raw data used fix the structure
-        Integer Expected = hashTableTest.find(1,4).getValue();
-
-        assertEquals(hashTableTest.find(1,4).getValue(),4);
+    public void setUp1(){
+        HashTableTesting();
+        hashTableTest.add(1,1);
+        hashTableTest.add(2,2);
+        hashTableTest.add(3,3);
     }
 
-
-        /*
 
     @Test
-    public void testAddMethodEmptyListGivenSizeCollisions(){
-        setUp2();
-        hashTableTest.add(1,2);
-        hashTableTest.add(1,4);
-        assertEquals(hashTableTest.find(1,4).getValue(),4);
+    void getValueTest(){
+        setUp1();
+        Integer result = hashTableTest.getValue(1);
+        assertEquals(1,result);
     }
 
-    public void setUp3(){
-        setUp();
+    @Test
+    void getFirstTest(){
+        setUp1();
+        HashEntry<Integer,Integer> result=hashTableTest.getFirst(1);
+        Integer expectedValue=1;
+        assertEquals(1,result.getValue());
+
+    }
+
+    public void setUp2(){
+        HashTableTesting();
         hashTableTest.add(1,1);
         hashTableTest.add(1,2);
         hashTableTest.add(2,1);
         hashTableTest.add(2,2);
-    }
-
-    @Test
-    public void testSearchMethodFirstPosition(){
-        setUp3();
-        assertEquals( hashTableTest.getFirst(1).getValue(),1);
-
-    }
-
-    @Test
-    public void testFindMethod(){
-        setUp3();
-        assertEquals( hashTableTest.find(1,2).getValue(),2);
-    }
-    @Test
-    public void testFindMethodNotExistingElement(){
-        setUp3();
-        assertNull(hashTableTest.find(1,5).getValue());
-    }
-    @Test
-    public void testFindMethodNotIndexElement(){
-        setUp3();
-        assertNull(hashTableTest.find(5,1).getValue());
-    }
-    @Test
-    public void testSearchElementEmptyTable(){
-        setUp();
-        assertNull(hashTableTest.getFirst(1));
-    }
-
-    @Test
-    public void testFindElementEmptyTable(){
-        setUp();
-        assertNull(hashTableTest.find(1,1));
-    }
-
-    public void setUp4(){
-        setUp();
-        hashTableTest.add(1,1);
-        hashTableTest.add(2,2);
-        hashTableTest.add(3,3);
-        hashTableTest.add(4,4);
+        hashTableTest.add(3,1);
+        hashTableTest.add(3,2);
     }
 
 
     @Test
-    public void deletionMethodIndexWithUniqueElement(){
-        setUp4();
+    void findValueTest() {
+        setUp2();
+        Integer result=hashTableTest.findValue(1,2);
+        assertEquals(2,result);
+    }
+
+    @Test
+    void findTest(){
+        setUp2();
+
+        HashEntry<Integer,Integer> result = hashTableTest.find(1,2);
+        Integer resultValue=result.getValue();
+        Integer expectedResult=2;
+
+        assertEquals(2, resultValue);
+    }
+    @Test
+    void deleteTest(){
+        setUp1();
         hashTableTest.delete(1,1);
-        assertNull(hashTableTest.getFirst(1));
-
-    }
-
-
-
-    @Test
-    public void deletionMethodCollisions(){
-        setUp3();
-        hashTableTest.delete(1,2);
-        assertNull(hashTableTest.find(1,2));
-
+        HashEntry<Integer,Integer> result=hashTableTest.getFirst(1);
+        assertNull(result);
     }
 
     @Test
-    public void deletionNotExistingElementIndexWithUniqueElement(){
-        setUp4();
-        hashTableTest.delete(1,2);
-        assertNotNull(hashTableTest.getFirst(1));
+    void isEmptyTest(){
+        HashTableTesting();
+        boolean result= hashTableTest.isEmpty();
+        assertTrue(result);
     }
-
     @Test
-    public void deletionNotExistingElementCollisions(){
-        setUp3();
-        hashTableTest.delete(1,5);
-        assertNotNull(hashTableTest.getFirst(1));
+    void isEmptyNegativeTest(){
+        setUp1();
+        boolean result= hashTableTest.isEmpty();
+        assertFalse(result);
     }
-
-*/
 
 
 
